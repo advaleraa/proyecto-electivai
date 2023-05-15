448d786184501b101db5bd0390875b666c67dd12
Angie Valera <avalera@mangus.org>
2023-05-15 07:38:20 -0500

    electiva disc

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,7 +6,22 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  getname(): string {
+    return this.appService.getname();
+  }
+
+  @Get('hello')
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('hello/:name')
+  getHelloAndName(@Param('name') name: string): string {
+    return this.appService.getHelloAndName(name);
+  }
+
+  @Get('products')
+  getproducts(@Query('limit') limit = 10, @Query('offset') offset = 0): string {
+    return this.appService.getproducts(limit, offset);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,16 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  getHelloAndName(name: string) {
+    return `Hello ${name}`;
+  }
+
+  getproducts(limit: number, offset: number) {
+    return `Mostrando el listado de productos desde el id: ${offset}. Cantidad a mostrar: ${limit}`;
+  }
+
+  getname() {
+    return 'Hola mi nombre es Angie';
+  }
 }
